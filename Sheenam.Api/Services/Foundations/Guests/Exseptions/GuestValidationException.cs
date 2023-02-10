14c5ac74ccac1f2ef4c20d48d0d501fc3ee9fd04
Azimjon <azimjon.007@mail.ru>
2023-02-10 18:46:26 +0200

    ShouldThrowValidationExseptionOnAddIfGuestIsNullAddLogItAsync ->WIP

---
+++ Sheenam.Api/Services/Foundations/Guests/Exseptions/GuestValidationException.cs
@@ -0,0 +1,18 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use Find Comfort and Peace
+//=================================================
+
+
+using Xeptions;
+
+namespace Sheenam.Api.Services.Foundations.Guests.Exseptions
+{
+    public class GuestValidationException: Xeption
+    {
+        public GuestValidationException(Xeption innerException)
+            : base(message: "Guest validation error occurred, fix the error and try again",
+                  innerException)
+        {}
+    }
+}
