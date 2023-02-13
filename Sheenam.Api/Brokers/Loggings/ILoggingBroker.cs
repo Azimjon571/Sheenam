2f480c71a0abdfe863e767a5d37f5c56ca6dd5e2
Azimjon <azimjon.007@mail.ru>
2023-02-13 13:27:45 +0200

    BROKERS: Implement Logging

---
+++ Sheenam.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use Find Comfort and Peace
+//=================================================
+
+using System;
+
+namespace Sheenam.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
