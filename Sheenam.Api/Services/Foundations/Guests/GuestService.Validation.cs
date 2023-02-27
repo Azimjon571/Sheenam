957e8fc31c5113f128d08968a0b8f03505ffb61a
Azimjon <azimjon.007@mail.ru>
2023-02-27 12:52:40 +0200

    ShouldThrowValidationExceptionOnAddIfGenderIsInvalidAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.Validation.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.Validation.cs
@@ -22,7 +22,8 @@ namespace Sheenam.Api.Services.Foundations.Guests
                 (Rule:IsInvalid(guest.LastName),Parameter:nameof(Guest.LastName)),
                 (Rule:IsInvalid(guest.DateOfBirth),Parameter:nameof(Guest.DateOfBirth)),
                 (Rule:IsInvalid(guest.Email),Parameter:nameof(Guest.Email)),
-                (Rule:IsInvalid(guest.Address),Parameter:nameof(Guest.Address)));
+                (Rule:IsInvalid(guest.Address),Parameter:nameof(Guest.Address)),
+                (Rule:IsInvalid(guest.Gender), Parameter:nameof(Guest.Gender)));
 
         }
         private void ValidateGuestNull(Guest guest)
@@ -38,6 +39,12 @@ namespace Sheenam.Api.Services.Foundations.Guests
         {
             Condition = id == Guid.Empty,
             Message="Id is required"
+        };
+
+        private static dynamic IsInvalid(GenderType gender) => new
+        {
+            Condition = Enum.IsDefined(gender) is false,
+            Message = "Value is invalid"
         };
 
         private static dynamic IsInvalid(string text) => new
