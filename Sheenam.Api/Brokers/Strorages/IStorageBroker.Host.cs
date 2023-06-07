812e7c4af7240ee07f725a465c2eb09c789a41ee
Azimjon571 <azimjon.007@mail.ru>
2023-06-07 14:11:29 +0500

    BROKERS: Insert Host

---
+++ Sheenam.Api/Brokers/Strorages/IStorageBroker.Host.cs
@@ -0,0 +1,16 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use Find Comfort and Peace
+//=================================================
+
+
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Hosts;
+
+namespace Sheenam.Api.Brokers.Strorages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<HoSt> InsertHostAsync(HoSt hoSt);
+    }
+}
