78673ba55c65667bfbd20d14e616f7af27f98bf0
Azimjon571 <azimjon.007@mail.ru>
2023-06-07 14:14:45 +0500

    CODERUD: StorageBroker.Hosts.cs

--- Sheenam.Api/Brokers/Strorages/StorageBroker.Hosts.cs
+++ Sheenam.Api/Brokers/Strorages/StorageBroker.Hosts.cs
@@ -1,4 +1,14 @@
+<<<<<<< Updated upstream
 ﻿using System.Threading.Tasks;
+=======
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use Find Comfort and Peace
+//=================================================
+
+
+using System.Threading.Tasks;
+>>>>>>> Stashed changes
 using Microsoft.EntityFrameworkCore;
 using Sheenam.Api.Models.Foundations.Hosts;
 
