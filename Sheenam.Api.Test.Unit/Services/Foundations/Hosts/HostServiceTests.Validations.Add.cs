27c1a1152c8861f81c382ca4050789765b185e86
Azimjon571 <azimjon.007@mail.ru>
2023-06-07 18:04:22 +0500

    ShouldThrowValidationExceptionOnAddIfHostIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Test.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
+++ Sheenam.Api.Test.Unit/Services/Foundations/Hosts/HostServiceTests.Validations.Add.cs
@@ -3,6 +3,9 @@
 // Free To Use Find Comfort and Peace
 //=================================================
 
+using FluentAssertions;
+using Microsoft.Extensions.Hosting;
+using System;
 using Moq;
 using Sheenam.Api.Models.Foundations.Hosts;
 using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
@@ -38,6 +41,69 @@ namespace Sheenam.Api.Test.Unit.Services.Foundations.Hosts
             this.storageBrokerMock.Verify(broker =>
                 broker.InsertHostAsync(It.IsAny<HoSt>()),
                     Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfHostIsInvalidAndLogItAsync(
+           string invalidString)
+        {
+            // given
+            var invalidHost = new HoSt
+            {
+                FistName = invalidString
+            };
+
+            var invalidHostException = new InvalidHostException();
+
+            invalidHostException.AddData(
+                key: nameof(HoSt.Id),
+                values: "Id is required");
+
+            invalidHostException.AddData(
+                key: nameof(HoSt.FistName),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+                key: nameof(HoSt.LastName),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+                key: nameof(HoSt.DateOfBirth),
+                values: "Value is required");
+
+            invalidHostException.AddData(
+                key: nameof(HoSt.Email),
+                values: "Text is required");
+
+            invalidHostException.AddData(
+                key: nameof(HoSt.PhoneNumber),
+                values: "Text is required");
+
+
+            var expectedHostValidationException =
+                new HostValidationException(invalidHostException);
+
+            // when
+            ValueTask<HoSt> addHostTask = this.hostService.AddHostAsync(invalidHost);
+
+            HostValidationException actualHostValidationException =
+                await Assert.ThrowsAsync<HostValidationException>(addHostTask.AsTask);
+
+            // then
+            actualHostValidationException.Should().BeEquivalentTo(expectedHostValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHostValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHostAsync(It.IsAny<HoSt>()), Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
