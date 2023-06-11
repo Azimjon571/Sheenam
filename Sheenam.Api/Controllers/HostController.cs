453dda2781781c539046609837a95bbcdf5dba84
Azimjon571 <azimjon.007@mail.ru>
2023-06-11 23:50:31 +0500

    CONTROLLERS: Post Host

---
+++ Sheenam.Api/Controllers/HostController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Sheenam.Api.Models.Foundations.Hosts;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
+using Sheenam.Api.Services.Foundations.Hosts;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HostController : RESTFulController
+    {
+        
+        private readonly IHostService hostService;
+
+        public HostController(IHostService hostService)=>
+            this.hostService = hostService;
+        [HttpPost]
+        public async ValueTask<ActionResult<HoSt>> PostHostAsync(HoSt hoSt)
+        {
+            try
+            {
+                HoSt postedHost = await this.hostService.AddHostAsync(hoSt);
+
+                return Created(postedHost);
+            }
+            catch (HostValidationException hostValidationException)
+            {
+                return BadRequest(hostValidationException.InnerException);
+            }
+            catch(HostDependencyValidationException hostDependencyValidationException)
+                when (hostDependencyValidationException.InnerException is AlreadyExistHostException)
+            {
+                return Conflict(hostDependencyValidationException.InnerException);
+            }
+            catch(HostDependencyValidationException hostDependencyValidationException)
+            {
+                return BadRequest(hostDependencyValidationException.InnerException);
+            }
+            catch(HostDependencyException hostDependencyException)
+            {
+                return InternalServerError(hostDependencyException.InnerException);
+            }
+            catch(HostServiceException hostServiceException)
+            {
+                return InternalServerError(hostServiceException.InnerException);
+            }
+        }
+    }
+}
